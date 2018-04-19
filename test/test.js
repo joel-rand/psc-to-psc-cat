0975b1882a5dafb2b80bee5ff73b3a0e74004a92
Joel Rand <joel_rand@mckinsey.com>
2018-04-19 09:20:03 -0500

    added some more tests

--- test/test.js
+++ test/test.js
@@ -1,25 +1,61 @@
 'user strict';
 
-var xs = require('..');
+var psc_mapping = require('../psc-mapping.json');
+var naics_mapping = require('../naics-mapping.json');
+
 var _ = require('lodash');
 var expect = require('chai').expect;
 
-describe('#xs',function() {
+describe('PSC Mapping Tests', function() {
 
-    it ('PSC title should be GUNS, THROUGH 30MM',function() {
-      var title = _.find(xs,{pscCode:'1005'}).pscTitle;
-      expect(title).to.equal('GUNS, THROUGH 30MM');
-    });
+  it('PSC title should be GUNS, THROUGH 30MM', function() {
+    var title = _.find(psc_mapping, {
+      pscCode: '1005'
+    }).pscTitle;
+    expect(title).to.equal('GUNS, THROUGH 30MM');
+  });
 
-    it ('Check record values',function() {
-      var record = _.find(xs,{pscCode:'AS46'});
-      expect(record.pscTitle).to.equal('R&D- MODAL TRANSPORTATION: MARINE (MANAGEMENT/SUPPORT)');
-      expect(record.pscCategoryCode).to.equal('A');
-      expect(record.pscCategoryTitle).to.equal('RESEARCH AND DEVELOPMENT');
-      expect(record.coiCode).to.equal('12');
-      expect(record.coiTitle).to.equal('GROUND AND SEA PLATFORMS (G&SP)');
-      expect(record.budgetActivityCode).to.equal('06');
-      expect(record.budgetActivityTitle).to.equal('RDT&E MANAGEMENT SUPPORT');
+  it('Checking PSC record AS46', function() {
+    var record = _.find(psc_mapping, {
+      pscCode: 'AS46'
     });
+    expect(record.pscTitle).to.equal('R&D- MODAL TRANSPORTATION: MARINE (MANAGEMENT/SUPPORT)');
+    expect(record.pscCategoryCode).to.equal('A');
+    expect(record.pscCategoryTitle).to.equal('RESEARCH AND DEVELOPMENT');
+    expect(record.coiCode).to.equal('12');
+    expect(record.coiTitle).to.equal('GROUND AND SEA PLATFORMS (G&SP)');
+    expect(record.budgetActivityCode).to.equal('06');
+    expect(record.budgetActivityTitle).to.equal('RDT&E MANAGEMENT SUPPORT');
+  });
+
+  it('Checking PSC record not found', function() {
+    var record = _.find(psc_mapping, {
+      pscCode: 'XXAAYY'
+    });
+    expect(record === undefined);
+  });
+
+  it('Checking NAICS record', function() {
+    var record = _.find(naics_mapping, {
+      naicsCode: '111140'
+    });
+    expect(record.naicsTitle).to.equal('WHEAT FARMING');
+    expect(record.pscCode).to.equal('8920');
+    expect(record.pscTitle).to.equal('BAKERY AND CEREAL PRODUCTS');
+  });
+
+  it('Checking NAICS record not found', function() {
+    var record = _.find(naics_mapping, {
+      naicsCode: 'XXAAYY'
+    });
+    expect(record === undefined);
+  });
+
+  it('Checking NAICS search by PSC 8920, should be 17 records', function() {
+    var records = _.filter(naics_mapping, {
+      pscCode: '8920'
+    });
+    expect(records.length).to.equal(17);
+  });
 
 });
